923a84d321332b312bd0676ca51e2b56ac81bbb4
chaeeun29 <codmsdl299@gmail.com>
2026-01-06 19:35:01 +0900

    01. tab-menu clear

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // tab메뉴 구현
+$(".tab_list").click(function(e){
+ e.preventDefault();
+// 클릭한 data-tab의 값 tabName에 대입
+ let tabName = $(this).data("tab");
+ $(".tab_list").removeClass("active");
+ $(this).addClass('active');
+ $(".pr_box").removeClass('active');
+ $("#" + tabName).addClass("active");
+});
